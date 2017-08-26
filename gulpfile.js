a2f49e42bc2f1f744986742dfd691bef5bfbd927
Márcio José Gurka Jr <marciogurkajr@gmail.com>
2017-08-26 16:15:49 -0300

    chore: unused tasks removed from gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -10,26 +10,8 @@ var gulp = require('gulp'),
 var paths = {
     scripts: ['dev/js/*.js'],
     styles: ['dev/css/*.css'],
-    content: ['dev/index.html', 'dev/project-2048.html', 
-    	'dev/project-mobile.html', 'dev/project-webperf.html'],
-    optScripts: ['dev/views/js/*.js'],
-    optStyles: ['dev/views/css/*.css'],
-    optContent: ['dev/views/opt.html']
+    content: ['dev/index.html']
 };
-
-/* Minifies our opt HTML files and outputs them to prod/*.html */
-gulp.task('optHTML', function() {
-    return gulp.src(paths.optContent)
-        .pipe(inline({
-    	base: paths.optContent,
-    	js: uglify,
-    	css: minifyCSS,
-    	disabledTypes: ['svg', 'img']
-        }))
-        .pipe(minifyHTML({ empty: true }))
-		.pipe(gulp.dest('prod/views/'));
-});
-
 
 /* Minifies our JS files and outputs them to prod/views/js */
 gulp.task('scripts', function(){
@@ -40,25 +22,10 @@ gulp.task('scripts', function(){
 		/* minify the file */
 		.pipe(uglify())
 		/* rename the file */
-		.pipe(rename('perfmatters.min.js'))
+		.pipe(rename('neighborhood.min.js'))
 		.pipe(plumber.stop())
 		/*save destination for minified file*/
 		.pipe(gulp.dest('prod/js/'));
-});
-
-/* Minifies our opt JS files and outputs them to prod/views/js */
-gulp.task('optJS', function(){
-	/*look for any file that has javascript & css in filename*/
-	gulp.src(paths.optScripts)
-		/* still run watch task even if error in code */
-		.pipe(plumber())
-		/*minify the file*/
-		.pipe(uglify())
-		/*rename the file*/
-		.pipe(rename('main.min.js'))
-		.pipe(plumber.stop())
-		/*save destination for minified file*/
-		.pipe(gulp.dest('prod/views/js/'));
 });
 
 /* Minifies our HTML files and outputs them to prod/*.html */
@@ -69,7 +36,7 @@ gulp.task('content', function() {
     	js: uglify,
     	css: minifyCSS,
     	disabledTypes: ['svg', 'img'],
-    	ignore: ['dev/js/perfmatters.js', 'http://www.google-analytics.com/analytics.js']
+    	ignore: ['dev/js/scripts.js', 'http://www.google-analytics.com/analytics.js']
         }))
         .pipe(minifyHTML({ empty: true }))
 		.pipe(gulp.dest('prod/'));
@@ -83,24 +50,12 @@ gulp.task('styles', function (){
 
 });
 
-/* Minifies opt CSS files */
-gulp.task('optCSS', function (){
-	gulp.src(paths.optStyles)
-		.pipe(minifyCSS())
-		.pipe(gulp.dest('prod/views/css/'));
-
-});
-
 /* run gulp tasks in background when changes are made to file */
 gulp.task('watch', function(){
 	gulp.watch(paths.scripts, ['scripts']);
 	gulp.watch(paths.styles, ['styles']);
 	gulp.watch(paths.content, ['content']);
-	gulp.watch(paths.optScripts, ['optJS']);
-	gulp.watch(paths.optStyles, ['optCSS']);
-	gulp.watch(paths.optContent ['optCSS']);
 });
 
 gulp.task('default', ['scripts', 'styles', 
-	'content', 'optCSS',
-	'optHTML', 'optJS', 'watch']);
+	'content', 'watch']);
